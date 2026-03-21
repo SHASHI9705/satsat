ea4a59311629add5820fff92898804857d73d937
SHASHI9705 <singhshashiranjan34@gmail.com>
2026-03-22 00:14:00 +0530

    ddddd

--- apps/satyam-fe/app/signin/page.tsx
+++ apps/satyam-fe/app/signin/page.tsx
@@ -48,7 +48,7 @@ const LoginPage: React.FC = () => {
         phone: user.phoneNumber,
       };
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/otp-login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/otp-login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
